0dddefbd40c18bd744982182639b968e2c935ba9
100798f <farhodbek.nomonov@mail.ru>
2022-11-10 11:32:48 +0500

    new project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Home from "./components/home";
+import "./assets/styles/styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;

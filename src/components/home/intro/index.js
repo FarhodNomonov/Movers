855675c72c09ec7effb98cf3fc58211a22bf4c0b
oybek <oybekgithub98@gmail.com>
2022-11-10 13:48:49 +0300

    worg page

--- src/components/home/intro/index.js
+++ src/components/home/intro/index.js
@@ -4,38 +4,6 @@ function Intro() {
   return (
     <nav className="px-2 bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
-        {/* <a href="#" className="flex items-center">
-          <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            className="mr-3 h-6 sm:h-10"
-            alt="Flowbite Logo"
-          />
-          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-            Flowbite
-          </span>
-        </a> */}
-        {/* <button
-          data-collapse-toggle="navbar-multi-level"
-          type="button"
-          className="inline-flex justify-center items-center ml-3 text-gray-400 rounded-lg md:hidden hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-300 dark:text-gray-400 dark:hover:text-white dark:focus:ring-gray-500"
-          aria-controls="navbar-multi-level"
-          aria-expanded="false"
-        >
-          <span className="sr-only">Open main menu</span>
-          <svg
-            className="w-6 h-6"
-            aria-hidden="true"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-              clip-rule="evenodd"
-            ></path>
-          </svg>
-        </button> */}
         <div
           className="hidden w-full md:block md:w-auto"
           id="navbar-multi-level"
--- src/components/works/index.js
+++ src/components/works/index.js
@@ -1,7 +1,35 @@
 import React from "react";
+import bgImg from "../../assets/images/Vector.png";
 
 function Works() {
-  return <div>Works</div>;
+  const backgroundImageStyle = {
+    backgroundImage: `url("${bgImg}")`,
+    backgroundSize: "cover",
+  };
+
+  return (
+    <div className=" text-white " style={backgroundImageStyle}>
+        <div className="bg-gradient-to-r from-black px-8 py-16">
+          <div className=" max-w-xl grid grid-cols-1 gap-8">
+            <div className="w-12">
+             
+            </div>
+            <h2 className="text-xl uppercase font-bold">Become a host</h2>
+            <h1 className="text-6xl font-bold">
+              Host your space, share your world
+            </h1>
+            <p className="text-lg">
+              The art of hosting is rooted in thoughtful design. Share your
+              unique aesthetic with guests and earn extra income on a schedule
+              that works for you.
+            </p>
+            <button className="bg-gradient-to-r from-pink-600 to-orange-600 py-3 px-6 text-lg rounded-md w-48">
+              Try hosting
+            </button>
+          </div>
+        </div>
+      </div>
+  );
 }
 
 export default Works;

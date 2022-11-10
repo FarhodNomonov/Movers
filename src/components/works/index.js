ebcc0a0531386e4e2a091408db2fde00a5ce4c57
oybek <oybekgithub98@gmail.com>
2022-11-10 16:12:12 +0300

    work page

--- src/components/works/index.js
+++ src/components/works/index.js
@@ -1,34 +1,78 @@
 import React from "react";
 import bgImg from "../../assets/images/Vector.png";
+import bgImgRight from "../../assets/images/Vector (2).png";
+import BannerImg from "../../assets/images/banner-img.png";
 
 function Works() {
   const backgroundImageStyle = {
     backgroundImage: `url("${bgImg}")`,
-    backgroundSize: "cover",
+    backgroundSize: "contain",
+    backgroundRepeat: "no-repeat",
+    width: "100%",
+    height: "21.5vw",
+  };
+
+  const backgroundImageStyle2 = {
+    backgroundImage: `url("${bgImgRight}")`,
+    backgroundSize: "contain",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "right",
+    width: "100%",
+    height: "704px",
   };
 
   return (
-    <div className=" text-white " style={backgroundImageStyle}>
-        <div className="bg-gradient-to-r from-black px-8 py-16">
-          <div className=" max-w-xl grid grid-cols-1 gap-8">
-            <div className="w-12">
-             
-            </div>
-            <h2 className="text-xl uppercase font-bold">Become a host</h2>
-            <h1 className="text-6xl font-bold">
-              Host your space, share your world
-            </h1>
-            <p className="text-lg">
-              The art of hosting is rooted in thoughtful design. Share your
-              unique aesthetic with guests and earn extra income on a schedule
-              that works for you.
-            </p>
-            <button className="bg-gradient-to-r from-pink-600 to-orange-600 py-3 px-6 text-lg rounded-md w-48">
-              Try hosting
-            </button>
+    <div className="works flex md:container md:mx-auto relative">
+      <div className="text-white w-3/12" style={backgroundImageStyle}>
+        <h1 className="text-5xl font-bold py-7 px-8 w-3/12 ">ПоРаботаем!</h1>
+      </div>
+      <div className="w-full overflow-hidden relative">
+        <div className="text-white" style={backgroundImageStyle2}>
+          <img
+            src={BannerImg}
+            alt="BannerImg"
+            className="cover right-1/4"
+            style={{
+              right: "-35%",
+              width: "718px",
+              height: "713px",
+              borderRadius: "50%",
+              position: "absolute",
+              top: "-2%",
+            }}
+          />
+        </div>
+      </div>
+      <div className="card flex justify-between">
+        <div className="card_item">
+          <div className="card_item__header">
+            <img src={BannerImg} alt="BannerImg" />
+          </div>
+          <div className="card_item__body justify-center">
+            <p>все виды погрузочно-разгрузочных работ</p>
+            <a href="#">От 200 рублей</a>
+          </div>
+        </div>
+        <div className="card_item">
+          <div className="card_item__header">
+            <img src={BannerImg} alt="BannerImg" />
+          </div>
+          <div className="card_item__body justify-center">
+            <p>все виды погрузочно-разгрузочных работ</p>
+            <a href="#">От 200 рублей</a>
+          </div>
+        </div>
+        <div className="card_item">
+          <div className="card_item__header">
+            <img src={BannerImg} alt="BannerImg" />
+          </div>
+          <div className="card_item__body justify-center">
+            <p>все виды погрузочно-разгрузочных работ</p>
+            <a href="#">От 200 рублей</a>
           </div>
         </div>
       </div>
+    </div>
   );
 }
 

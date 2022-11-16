e9c11b0da4df884e275ea52efa87f1ea7ade995c
Mahmudjon <mahmidjonasqarov101@gmail.com>
2022-11-16 16:33:25 +0500

    added responsive Service

--- src/components/service/Service.js
+++ src/components/service/Service.js
@@ -2,34 +2,34 @@ import React from "react";
 import "./Service.css";
 function Service() {
   return (
-    <div className=" md:container md:mx-auto w-full p-12 ">
+    <div className=" md:min-w-[590px]  max-w-[1536px] mx-auto w-full p-12 ">
       <div className="wrapper_title">
         <h2 className="title_pages text-[#28438E] my-3 mb-4 text-bold text-base font-bold font-Inter">
           СТОИМОСТЬ УСЛУГ
         </h2>
       </div>
-      <div className="container striped">
-        <div className="row  flex justify-between p-4 px-20 font-semibold		">
+      <div className=" striped">
+        <div className="row  flex justify-between p-4 px-20 md:px-4 font-semibold		">
           <h2>Услуга грузчиков</h2>
           <span>от 250 руб./час</span>
         </div>
-        <div className="row  flex justify-between p-4 px-20 font-semibold	">
+        <div className="row  flex justify-between p-4 px-20 md:px-4 font-semibold	">
           <h2>Услуга грузчиков</h2>
           <span>от 250 руб./час</span>
         </div>
-        <div className="row  flex justify-between p-4 px-20 font-semibold	">
+        <div className="row  flex justify-between p-4 px-20 md:px-4 font-semibold	">
           <h2>Услуга грузчиков</h2>
           <span>от 250 руб./час</span>
         </div>
-        <div className="row  flex justify-between p-4 px-20 font-semibold	">
+        <div className="row  flex justify-between p-4 px-20 md:px-4 font-semibold	">
           <h2>Услуга грузчиков</h2>
           <span>от 250 руб./час</span>
         </div>
-        <div className="row  flex justify-between p-4 px-20 font-semibold	">
+        <div className="row  flex justify-between p-4 px-20 md:px-4 font-semibold	">
           <h2>Услуга грузчиков</h2>
           <span>от 250 руб./час</span>
         </div>
-         <div className="row  flex justify-between p-4 px-20 font-semibold	">
+        <div className="row  flex justify-between p-4 px-20 md:px-4 font-semibold	">
           <h2>Услуга грузчиков</h2>
           <span>от 250 руб./час</span>
         </div>

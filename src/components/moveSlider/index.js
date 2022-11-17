390d3c01891acb1697ad314e144e72c3ab20d966
100798f <farhodbek.nomonov@mail.ru>
2022-11-17 14:19:57 +0500

    build(deps): update 2 files and delete 1 file

--- src/components/moveSlider/index.js
+++ src/components/moveSlider/index.js
@@ -12,7 +12,7 @@ function MoveSlider() {
     slidesToShow: window.innerWidth > 768 ? 2 : 1,
     slidesToScroll: 2,
   };
-  
+
   return (
     <div className="max-w-[1536px] h-max  mx-auto p-6 moveSlider sm:min-w-[610px] ">
       <div className="wrapper_title ml-6">
@@ -22,6 +22,31 @@ function MoveSlider() {
       </div>
       <div className="cardItem h-max  ">
         <Slider {...settings}>
+          <div className="cardSlider">
+            <div className="cardSlider__titte flex justify-start items-center">
+              <div className="min-w-24  w-24 min-h-24  h-24">
+                <img className="rounded-full" src={UserPng} alt="UserPng" />
+              </div>
+              <div className="cardSlider__text">
+                <p>Дмитрий Филимонов</p>
+                <div className="stars flex justify-start items-center">
+                  <img src={StarPng} alt="StarPng" />
+                  <img src={StarPng} alt="StarPng" />
+                  <img src={StarPng} alt="StarPng" />
+                  <img src={StarPng} alt="StarPng" />
+                  <img src={StarPng} alt="StarPng" />
+                </div>
+              </div>
+            </div>
+            <div className="cardSlider__deck">
+              Дмитрий Филимонов Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat. Duis aute irure dolor in reprehenderit in voluptate
+              velit esse{" "}
+            </div>
+          </div>
           <div className="cardSlider">
             <div className="cardSlider__titte flex justify-start items-center">
               <div className="min-w-24  w-24 min-h-24  h-24">
